553f213ee6c2dce2a3146a85b9e9bf069a8a29db
BYN13K <64960366+BYN13K@users.noreply.github.com>
2020-09-24 20:23:29 +0200

    Q8-L2

---
+++ zhiwehu_Python_exercises/Q8.py
@@ -0,0 +1,32 @@
+"""
+### Question 8
+Level 2
+
+Question:
+Write a program that accepts a comma separated sequence of words as input and prints the words in a comma-separated sequence after sorting them alphabetically.
+Suppose the following input is supplied to the program:
+without,hello,bag,world
+Then, the output should be:
+bag,hello,without,world
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+
+"""
+
+def sorting(words):
+    if " " in words:
+        words = words.replace(" ","")
+    list_words = words.split(",")
+    list_words.sort()
+    print(",".join(list_words))
+
+a = "without,hello,bag,world"
+
+sorting(a)
+
+b = input("Input words separated whit comas: ")
+
+sorting(b)
+
+
